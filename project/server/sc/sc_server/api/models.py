75d0d3c0daac0daa8116c262f7423cfd3ab0afed
Lee Seunghwan <tmdghks304@gmail.com>
2023-09-11 14:34:11 +0900

    [FEAT] : post api 틀 작성

--- project/server/sc/sc_server/api/models.py
+++ project/server/sc/sc_server/api/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class image(models.Model):
+    image_url = models.CharField(max_length=1000)
--- project/server/sc/sc_server/status_csf/urls.py
+++ project/server/sc/sc_server/status_csf/urls.py
@@ -1,22 +1,32 @@
-"""
-URL configuration for status_csf project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import path, re_path
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+from rest_framework import routers
+from api import views
+
+from api.views import *
+
+# http://127.0.0.1:8000/swagger/
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="Stauts Classfication",
+        default_version='v1',
+        description="책 이미지 상태 분류 API",
+        terms_of_service="",
+    ),
+    public=True,
+    permission_classes=(AllowAny,), 
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/sc/test', test_api, name='test_api'),
+
+    # swagger
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
