ede2bed09501892460aa06a3e6052afd7ba22a39
Lee Seunghwan <tmdghks304@gmail.com>
2023-09-12 09:29:23 +0900

    [REMOVE] : 기존 프로젝트 삭제 설정 문제가 발생하여 기존 프로젝트 삭제 후 프로젝트 재시작

--- project/server/sc/sc_server/api/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class image(models.Model):
-    image_url = models.CharField(max_length=1000)
--- project/server/sc/sc_server/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.core import serializers
-from rest_framework import serializers
-from .models import image
-
-class image_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = image
-        fields = ['image_url']
-    image_url = serializers.CharField(max_length=1000)
