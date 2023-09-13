2b320354bfaf3800b4bb5971fb26ddd382c5669b
허재 <huhwo00@gmail.com>
2023-09-13 17:04:46 +0900

    [feat] 마리아db연동을 위한 코드 수정

--- project/server/auth/src/main/java/com/cc/auth/domain/member/domain/Member.java
+++ project/server/auth/src/main/java/com/cc/auth/domain/member/domain/Member.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 
 @Entity(name= "member")
@@ -13,8 +15,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-//@SQLDelete(sql = "UPDATE member SET activated = false WHERE member_id = ?")
-//@Where(clause = "activated = true")
+@SQLDelete(sql = "UPDATE member SET activated = false WHERE member_id = ?")
+@Where(clause = "activated = true")
 public class Member extends BaseTimeEntity {
 
     @Id
