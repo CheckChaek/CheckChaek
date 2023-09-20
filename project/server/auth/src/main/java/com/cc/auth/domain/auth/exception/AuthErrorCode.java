4fec515c467c3db6d6b64ca9a3d676b2d69e6d36
허재 <huhwo00@gmail.com>
2023-09-20 10:20:31 +0900

    [FEAT] 토큰유효성 검사 통과 이후 db에 해당 identifier가 없을 경우 필터단에서 에러를 던지도록 수정

--- project/server/auth/src/main/java/com/cc/auth/domain/auth/exception/AuthErrorCode.java
+++ project/server/auth/src/main/java/com/cc/auth/domain/auth/exception/AuthErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthErrorCode {
 
-    NOT_FOUND_OAUTH_IDENTIFIER(HttpStatus.NOT_FOUND, "oauthIdentifier가 없습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "일치하는 회원이 없습니다."),
     NOT_PROPER_TOKEN(HttpStatus.UNAUTHORIZED, "올바른 형태의 토큰이 아닙니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다.");
 
