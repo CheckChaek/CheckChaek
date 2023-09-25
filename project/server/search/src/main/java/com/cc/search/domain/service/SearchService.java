1b1a7bf59e6dc7ca816ef15ced0ba6c997255a30
JaeWooJeon <kyt9600@naver.com>
2023-09-25 14:07:06 +0900

    [FIX] certainBook 검색 시 filter를 통해 특정 책을 선별 하는 코드 추가

--- project/server/search/src/main/java/com/cc/search/domain/service/SearchService.java
+++ project/server/search/src/main/java/com/cc/search/domain/service/SearchService.java
@@ -2,11 +2,12 @@ package com.cc.search.domain.service;
 
 import com.cc.search.domain.dto.BookDto;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface SearchService {
 
     BookDto getBookInfo(List<String> textList);
 
-    BookDto searchCertainBookInfo(String keyword);
+    BookDto searchCertainBookInfo(HashMap<String, Object> request);
 }
