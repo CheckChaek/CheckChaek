275c14881eb929d2d2c2a02109bae67bf3bf5ecf
허재 <huhwo00@gmail.com>
2023-10-03 18:33:01 +0900

    [FEAT] openfeign reaname및 반환타입 수정

--- project/server/business/src/main/java/com/cc/business/global/config/filter/openfeign/AuthOpenFeign.java
+++ project/server/business/src/main/java/com/cc/business/global/config/filter/openfeign/AuthOpenFeign.java
@@ -1,15 +1,16 @@
-package com.cc.business.domain.controller.openfeign;
+package com.cc.business.global.config.filter.openfeign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="AuthOpenFeign", url = "http://j9a606.p.ssafy.io:8081")
+@FeignClient(name="AuthOpenFeign", url = "http://localhost:8081")
 public interface AuthOpenFeign {
 
     @GetMapping("/authorization")
-    int connectToAuthServer(
+    ResponseEntity<Integer> connectToAuthServer(
             @RequestHeader String Authorization,
-            @RequestHeader String AuthorizationRefresh
+            @RequestHeader String Authorization_refresh
     );
 }
