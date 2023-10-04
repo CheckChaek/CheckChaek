c5b83f2b77d0dec8f480550493af8b90cd75b190
허재 <huhwo00@gmail.com>
2023-10-04 13:25:21 +0900

    [FIX] CorsFilter 테스트

---
+++ project/server/business/src/main/java/com/cc/business/global/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.cc.business.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Slf4j
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        log.info("corsFilter 적용..");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> registrationBean =
+                new FilterRegistrationBean<>(corsFilter());
+        registrationBean.setOrder(0); // CorsFilter를 가장 먼저 실행하도록 설정
+        return registrationBean;
+    }
+}
