a83cba045635f121e58e69dedf61fde47980c867
김보석 <st02045@naver.com>
2023-10-05 10:11:42 +0900

    [DESING] 오류페이지 css 재수정

--- project/client/frontend/src/pages/errorPage.tsx
+++ project/client/frontend/src/pages/errorPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import DoubleRightIcon from '../assets/icons/dobuleRightIcon';
 import error from '../assets/images/error_page/error.png';
 
 function ErrorPage() {
@@ -10,14 +9,11 @@ function ErrorPage() {
         <div className="flex items-center justify-center pt-[10%]">
           <img src={error} alt="error" className="w-1/2 h-[55%] " />
           <div className="text-5xl font-do-hyeon">
-            <p>오류가 발생했습니다.</p>
-            <p className="mt-4">걱정하지마세요. 금방 해결 될거에요!</p>
-            <div className="mt-4">
-              <span>An </span>
-              <span className="text-LOGO-500">error </span>
-              <span>occured!</span>
-              <p className="my-4">It will be resolved soon.</p>
-            </div>
+            <p>
+              <span className="text-LOGO-500">오류</span>가 발생했습니다.
+            </p>
+            <p className="mt-4">걱정하지마세요.</p>
+            <p className="mt-4"> 금방 해결 될거에요!</p>
           </div>
         </div>
         <button
@@ -26,18 +22,6 @@ function ErrorPage() {
           onClick={() => navigate('/')}>
           메인으로 돌아가기
         </button>
-      </div>
-      <div className="flex">
-        <div className="w-[150vh]" />
-        <div className="flex text-2xl left-[80%] top-[85%] font-do-hyeon">
-          <div>
-            <p>문제를 해결해주세요!</p>
-            <p>I need any help!</p>
-          </div>
-          <button type="button" onClick={() => navigate('/')}>
-            <DoubleRightIcon styleString="h-14 w-16 mt-2 ml-3 text-SECONDARY-400 animate-pulse" />
-          </button>
-        </div>
       </div>
     </div>
   );
